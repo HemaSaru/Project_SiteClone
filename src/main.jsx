721fb670e43efb31409e179f1b9d19e5c56b1da5
HemaThapa <hemasaruthapa@gmail.com>
2024-03-11 23:58:44 +0530

    added hero img and service

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Navbar from './sections/Nav.jsx' 
+import Services from  "./sections/Services.jsx"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Navbar />
     <App />
+    <Services />
   </React.StrictMode>,
 )
